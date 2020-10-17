47bddfb143fb9b13607f64d65236ec5cf229abe3
Hugo Gresse <hugo.gresse@gmail.com>
2020-10-17 20:28:32 +0200

    Revert "Rewrite first category list using react virtualized"
    This reverts commit 86d24f3d

--- src/PackageCategoriesList.tsx
+++ src/PackageCategoriesList.tsx
@@ -1,85 +1,40 @@
 import React from 'react'
 import {Grid, Typography} from '@material-ui/core'
+import PackageItem from './PackageItem'
 import {PackagesByCategory} from '../functions/src/types'
 import {makeStyles} from '@material-ui/core/styles'
-import {WindowScroller, AutoSizer, List, ListRowRenderer, CellMeasurer, CellMeasurerCache} from 'react-virtualized'
-import PackageList from './PackageList'
 
 type PackageCategoriesList = {
     packagesByCategories?: PackagesByCategory[]
 }
 
 const useStyles = makeStyles(theme => ({
-    container: {
-        width: "100%"
-    },
     title: {
         marginTop: 64,
-    }
+    },
 }))
-
-const cache = new CellMeasurerCache({
-    defaultHeight: 500,
-    fixedWidth: true
-})
 
 
 const PackageCategoriesList = ({packagesByCategories}: PackageCategoriesList) => {
     const classes = useStyles()
 
-    if (!packagesByCategories) {
+    if(!packagesByCategories) {
         return <></>
     }
 
-    const rowRender: ListRowRenderer = ({index, key, style, parent}) => {
-        const packageByCategory = packagesByCategories[index]
-
-        return <CellMeasurer
-            cache={cache}
-            columnIndex={0}
-            key={key}
-            overscanRowCount={10}
-            parent={parent}
-            rowIndex={index}
-        >
-            <Grid item key={key} style={style}>
+    return <>{
+        packagesByCategories.map(({category, packages}) => {
+            return <Grid item key={category.key}>
                 <Typography variant="h1" className={classes.title}>
-                    {packageByCategory.category.name}
+                    {category.name}
                 </Typography>
-                <PackageList packages={packageByCategory.packages}/>
+                <Grid container>
+                    {packages.map(p => <PackageItem packageItem={p} key={p.name}/>)}
+                </Grid>
             </Grid>
-        </CellMeasurer>
+        })
     }
-
-    return <div className={classes.container}>
-        <WindowScroller
-            scrollElement={window}>
-            {({height, isScrolling, registerChild, onChildScroll, scrollTop}) => (
-                <div>
-                    {console.log("re der list", height, isScrolling, scrollTop)}
-                    <AutoSizer disableHeight>
-                        {({width}) => (
-                            <div ref={registerChild}>
-                                <List
-                                    autoHeight
-                                    height={height}
-                                    isScrolling={isScrolling}
-                                    onScroll={onChildScroll}
-                                    overscanRowCount={2}
-                                    rowCount={packagesByCategories.length}
-                                    rowHeight={cache.rowHeight}
-                                    rowRenderer={rowRender}
-                                    scrollTop={scrollTop}
-                                    width={width}
-                                />
-                            </div>
-                        )}
-                    </AutoSizer>
-                </div>
-            )}
-        </WindowScroller>
-
-    </div>
+    </>
 }
 
 export default PackageCategoriesList
